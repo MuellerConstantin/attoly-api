e642a3b930bb0c1e03eb081b7489f705939dbedd
Constantin Müller <32311121+0x1C1B@users.noreply.github.com>
2022-10-21 12:32:40 +0200

    feat: defines the basis for entities and persistence
    Defines a base entity with basic fields of metadata and an abstract persistence interface tailored to the base entity. The persistence of the data and the modeling of the domain is implemented using Hibernate/JPA.

---
+++ src/main/java/de/x1c1b/attoly/api/config/PersistenceConfig.java
@@ -0,0 +1,9 @@
+package de.x1c1b.attoly.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class PersistenceConfig {
+}
---
+++ src/main/java/de/x1c1b/attoly/api/domain/model/BaseEntity.java
@@ -0,0 +1,46 @@
+package de.x1c1b.attoly.api.domain.model;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.Type;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.UUID;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@SuperBuilder
+public abstract class BaseEntity implements Serializable {
+
+    @Id
+    @Type(type = "uuid-char")
+    @GeneratedValue
+    @Column(name = "id", unique = true, nullable = false)
+    private UUID id;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "last_modified_at", nullable = false)
+    private Instant lastModifiedAt;
+
+    @Version
+    @Column(name = "version", nullable = false)
+    @Setter(AccessLevel.PROTECTED)
+    @Builder.Default
+    private long version = 0L;
+
+    @Column(name = "deleted", nullable = false)
+    @Builder.Default
+    private boolean deleted = false;
+}
