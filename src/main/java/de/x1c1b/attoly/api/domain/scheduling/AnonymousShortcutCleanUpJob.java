44bf85d377db3c9f63de1ffa60e944c504d98766
Constantin Müller <32311121+0x1C1B@users.noreply.github.com>
2022-10-28 13:34:23 +0200

    feat: add cron job for deleting expired shortcuts
    Adds a cron job that deletes expired shortcuts that are not assigned to any account after a specified period of time.

---
+++ src/main/java/de/x1c1b/attoly/api/domain/scheduling/AnonymousShortcutCleanUpJob.java
@@ -0,0 +1,42 @@
+package de.x1c1b.attoly.api.domain.scheduling;
+
+import de.x1c1b.attoly.api.domain.impl.ShortcutServiceImpl;
+import de.x1c1b.attoly.api.repository.ShortcutRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+/**
+ * Cron job for deleting all expired and anonymously created shortcuts. This
+ * operation deletes all shortcuts that were not assigned to an account and
+ * which were created before a certain point in time, thus cleaning up the
+ * database.
+ */
+@Component
+@ConditionalOnProperty(value = "attoly.scheduling.jobs.anonymous-shortcut-clean-up.enabled", matchIfMissing = true, havingValue = "true")
+public class AnonymousShortcutCleanUpJob {
+
+    private final Logger logger = LoggerFactory.getLogger(ShortcutServiceImpl.class);
+
+    private final ShortcutRepository shortcutRepository;
+    private final long anonymousShortcutExpiresIn;
+
+    @Autowired
+    public AnonymousShortcutCleanUpJob(ShortcutRepository shortcutRepository,
+                                       @Value("${attoly.scheduling.jobs.anonymous-shortcut-clean-up.expires-in:2419200000}") long anonymousShortcutExpiresIn) {
+        this.shortcutRepository = shortcutRepository;
+        this.anonymousShortcutExpiresIn = anonymousShortcutExpiresIn;
+    }
+
+    @Scheduled(cron = "${attoly.scheduling.jobs.anonymous-shortcut-clean-up.cron:@daily}")
+    protected void run() {
+        logger.info("Deletion job for anonymous expired shortcuts is running");
+        shortcutRepository.deleteAllAnonymousSoftCreatedBefore(Instant.now().minusMillis(anonymousShortcutExpiresIn));
+    }
+}
